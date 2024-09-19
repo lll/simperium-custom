147864c234a6e3197f6d70823bcd39367e70b94b
Daniel Gomes Vargas <dgv@fastmail.com>
2024-09-18 22:13:57 -0300

    added authUrl

--- src/simperium/auth.js
+++ src/simperium/auth.js
@@ -1,11 +1,11 @@
 // @flow
-import events from 'events'
-import request from './http-request';
+import events from "events";
+import request from "./http-request";
 
 // @flow
 type User = {
-	options: {},
-	access_token: string,
+  options: {},
+  access_token: string,
 };
 
 const fromJSON = (json: string): User => {
@@ -15,83 +15,88 @@ const fromJSON = (json: string): User => {
   } catch (error) {
     throw new Error(json);
   }
-  if (!data.access_token && typeof data.access_token !== 'string') {
-    throw new Error('access_token not present');
+  if (!data.access_token && typeof data.access_token !== "string") {
+    throw new Error("access_token not present");
   }
-	return {
-		options: data,
-		access_token: new String( data.access_token ).toString()
-	};
+  return {
+    options: data,
+    access_token: new String(data.access_token).toString(),
+  };
 };
 
 const { EventEmitter } = events;
 
-const baseUrl = 'https://auth.simperium.com/1';
-
 export class AuthError extends Error {
-	underlyingError: Error
+  underlyingError: Error;
 
-	constructor( underlyingError: Error ) {
-		super( 'Failed to authenticate user.' );
-		this.underlyingError = underlyingError;
-	}
+  constructor(underlyingError: Error) {
+    super("Failed to authenticate user.");
+    this.underlyingError = underlyingError;
+  }
 }
 
 /**
  * Client for creating and authenticating Simperium.com user accounts.
  */
 export class Auth extends EventEmitter {
-	appId: string
-	appSecret: string
+  appId: string;
+  appSecret: string;
+  authUrl: string;
 
-	/**
-	 * Creates an instance of the Auth client
-	 *
-	 * @param {string} appId - Simperium.com application ID
-	 * @param {string} appSecret - Simperium.com application secret
-	 */
-	constructor( appId: string, appSecret: string ) {
-		super();
-		this.appId = appId;
-		this.appSecret = appSecret;
-	}
+  /**
+   * Creates an instance of the Auth client
+   *
+   * @param {string} appId - Simperium.com application ID
+   * @param {string} appSecret - Simperium.com application secret
+   * @param {string} authUrl - Simperium.com application authentication URL
+   */
+  constructor(appId: string, appSecret: string, authUrl: string) {
+    super();
+    this.appId = appId;
+    this.appSecret = appSecret;
+    this.authUrl = authUrl;
+  }
 
-	/**
-	 * Authorizes a user account with username and password
-	 *
-	 * @param {string} username account username
-	 * @param {string} password account password
-	 * @returns {Promise<User>} user account data
-	 */
-	authorize( username: string, password: string ) {
-		const body = JSON.stringify( { username: username, password: password } );
-		return this.request( 'authorize/', body );
-	}
+  /**
+   * Authorizes a user account with username and password
+   *
+   * @param {string} username account username
+   * @param {string} password account password
+   * @returns {Promise<User>} user account data
+   */
+  authorize(username: string, password: string) {
+    const body = JSON.stringify({
+      username: username,
+      password: password,
+    });
+    return this.request("authorize/", body);
+  }
 
-	create( username: String, password: String, provider: ?String ) {
-		const userData: { username: String, password: String, provider?: String } = { username, password };
-		if ( provider ) {
-			userData.provider = provider;
-		}
-		const body = JSON.stringify( userData );
-		return this.request( 'create/', body );
-	}
+  create(username: String, password: String, provider: ?String) {
+    const userData: { username: String, password: String, provider?: String } =
+      { username, password };
+    if (provider) {
+      userData.provider = provider;
+    }
+    const body = JSON.stringify(userData);
+    return this.request("create/", body);
+  }
 
-	request( endpoint: string, body: string ): Promise<User> {
-		const authUrl = `${ baseUrl }/${ this.appId }/${ endpoint }`;
+  request(endpoint: string, body: string): Promise<User> {
+    const authUrl = `${this.authUrl}/${this.appId}/${endpoint}`;
 
-		return request( this.appSecret, authUrl, body ).then( response => {
-			try {
-				const user = fromJSON( response );
-				this.emit( 'authorize', user );
-				return user;
-			} catch ( error ) {
-				throw new AuthError( error );
-			}
-		} )
-	}
+    return request(this.appSecret, authUrl, body).then((response) => {
+      try {
+        const user = fromJSON(response);
+        this.emit("authorize", user);
+        return user;
+      } catch (error) {
+        throw new AuthError(error);
+      }
+    });
+  }
 }
 
-export default ( appId: string, appSecret: string ) => {
-	return new Auth( appId, appSecret );
+export default (appId: string, appSecret: string, authUrl: string) => {
+  return new Auth(appId, appSecret, authUrl);
 };
--- test/simperium/auth_test.js
+++ test/simperium/auth_test.js
@@ -1,96 +1,92 @@
-import buildAuth from '../../src/simperium/auth'
-import https from 'https'
-import { equal } from 'assert'
-import { EventEmitter } from 'events'
+import buildAuth from "../../src/simperium/auth";
+import https from "https";
+import { equal } from "assert";
+import { EventEmitter } from "events";
 
 const originalRequest = https.request;
-const stub = ( respond ) => {
-	https.request = ( url, options, handler ) => {
-		const req = new EventEmitter()
-		req.end = ( body ) => respond( body, handler )
-		return req
-	}
-}
+const stub = (respond) => {
+  https.request = (url, options, handler) => {
+    const req = new EventEmitter();
+    req.end = (body) => respond(body, handler);
+    return req;
+  };
+};
 
-const stubResponse = ( data ) => stub( ( body, handler ) => {
-	const response = new EventEmitter()
-	handler( response )
-	response.emit( 'data', data )
-	response.emit( 'end' )
-} )
+const stubResponse = (data) =>
+  stub((body, handler) => {
+    const response = new EventEmitter();
+    handler(response);
+    response.emit("data", data);
+    response.emit("end");
+  });
 
-describe( 'Auth', () => {
-	let auth;
+describe("Auth", () => {
+  let auth;
 
-	after( () => {
-		// Unstub it
-		https.request = originalRequest;
-	} );
+  after(() => {
+    // Unstub it
+    https.request = originalRequest;
+  });
 
-	beforeEach( () => {
-		auth = buildAuth( 'token', 'secret' );
-	} );
+  beforeEach(() => {
+    auth = buildAuth("token", "secret", "url");
+  });
 
-	it( 'should request auth token', () => {
-		stub( ( data, handler ) => {
-			const { username, password } = JSON.parse( data )
-			const response = new EventEmitter()
-			equal( username, 'username' )
-			equal( password, 'password' )
+  it("should request auth token", () => {
+    stub((data, handler) => {
+      const { username, password } = JSON.parse(data);
+      const response = new EventEmitter();
+      equal(username, "username");
+      equal(password, "password");
 
-			handler( response )
-			response.emit( 'data', '{\"access_token\": \"secret-token\"}' )
-			response.emit( 'end' );
-		} )
+      handler(response);
+      response.emit("data", '{"access_token": "secret-token"}');
+      response.emit("end");
+    });
 
-		return auth.authorize( 'username', 'password' )
-			.then( ( user ) => {
-				equal( user.access_token, 'secret-token' );
-			} );
-	} );
+    return auth.authorize("username", "password").then((user) => {
+      equal(user.access_token, "secret-token");
+    });
+  });
 
-	it( 'should fail if missing access_token', () => {
-		stubResponse( '{"hello":"world"}' );
-		return auth.authorize( 'username', 'password' )
-			.catch( error => {
-				equal( error.message, 'Failed to authenticate user.' );
-				equal( error.underlyingError.message, 'access_token not present' );
-			} );
-	} );
+  it("should fail if missing access_token", () => {
+    stubResponse('{"hello":"world"}');
+    return auth.authorize("username", "password").catch((error) => {
+      equal(error.message, "Failed to authenticate user.");
+      equal(error.underlyingError.message, "access_token not present");
+    });
+  });
 
-	it( 'should fail to auth with invalid credentials', () => {
-		stubResponse( 'this is not json' )
+  it("should fail to auth with invalid credentials", () => {
+    stubResponse("this is not json");
 
-		return auth.authorize( 'username', 'bad-password' )
-			.catch( ( e ) => {
-				equal( e.message, 'Failed to authenticate user.' );
-			} );
-	} )
+    return auth.authorize("username", "bad-password").catch((e) => {
+      equal(e.message, "Failed to authenticate user.");
+    });
+  });
 
-	it( 'should create an account with valid credentials', () => {
-		stub( ( data, handler ) => {
-			const { username, password } = JSON.parse( data )
-			const response = new EventEmitter()
-			equal( username, 'username' )
-			equal( password, 'password' )
+  it("should create an account with valid credentials", () => {
+    stub((data, handler) => {
+      const { username, password } = JSON.parse(data);
+      const response = new EventEmitter();
+      equal(username, "username");
+      equal(password, "password");
 
-			handler( response )
-			response.emit( 'data', '{\"access_token\": \"secret-token\"}' )
-			response.emit( 'end' );
-		} )
+      handler(response);
+      response.emit("data", '{"access_token": "secret-token"}');
+      response.emit("end");
+    });
 
-		return auth.create( 'username', 'password' )
-			.then( user => {
-				equal( user.access_token, 'secret-token' )
-			} );
-	} )
+    return auth.create("username", "password").then((user) => {
+      equal(user.access_token, "secret-token");
+    });
+  });
 
-	it( 'should fail to create an account with invalid credentials', () => {
-		stubResponse( 'this is not json' )
+  it("should fail to create an account with invalid credentials", () => {
+    stubResponse("this is not json");
 
-		return auth.create( 'username', 'bad-password' )
-			.catch( ( e ) => {
-				equal( e.message, 'Failed to authenticate user.' );
-			} );
-	} )
-} )
+    return auth.create("username", "bad-password").catch((e) => {
+      equal(e.message, "Failed to authenticate user.");
+    });
+  });
+});
